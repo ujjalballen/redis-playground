493ba3d216c5933d8d10f0ff47914c040603d1e2
Ujjal B. Allen <ujjal.vision02@gmail.com>
2026-01-31 01:09:11 +0600

    understand lots with demo data

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.get('/', async (req, res) => {
 
     const requestCount = await client.incr(clientIP);
 
-    if(requestCount === 2){
+    if (requestCount === 2) {
         await client.expire(clientIP, 60)
     }
 
@@ -44,7 +44,7 @@ app.get('/', async (req, res) => {
 
 
 
-    if(requestCount > 5){
+    if (requestCount > 5) {
         return res.status(409).send(`Too many request: ${timeLeave}`)
     }
 
@@ -117,7 +117,7 @@ app.get("/order/:id", async (req, res) => {
 
 // LISTS
 
-app.get("/products/add/:id", async(req, res) => {
+app.get("/products/add/:id", async (req, res) => {
     const productId = req.params.id;
 
     const getProduct = getProductOne(productId);
@@ -128,7 +128,7 @@ app.get("/products/add/:id", async(req, res) => {
 
     // await client.RPOP("products")
 
-//    const final = await client.LLEN("products")
+    //    const final = await client.LLEN("products")
 
     // await client.LPOP("products")
 
@@ -137,10 +137,86 @@ app.get("/products/add/:id", async(req, res) => {
 
     const result = [getProduct];
 
-    res.json({result: getAll})
+    res.json({ result: getAll })
 
 })
 
+
+
+// Sets
+app.get("/product/sets/:id", async (req, res) => {
+    const id = req.params.id;
+
+    // const getProduc = getProductOne(id);
+
+    // await client.SADD("product", id)
+
+    // const result = await client.sAdd("bikes:racing:france", ['bike:2', 'bike:3'])
+
+    // const result = await client.del("bikes:racing:france")
+    // const result= await client.sRem("product", "1")
+    const result = await client.sIsMember("product", "2120")
+
+
+    res.json({ result })
+
+});
+
+
+// hashes
+app.get("/hashes", async (req, res) => {
+    const product = { id: 1, name: "Redis Cookbook", price: 29.99 };
+
+    // const result = await client.hSet("products", product);
+    // const result = await client.hGet("products", "price")
+    // const result = await client.hGetAll("products")
+    // const result = await client.hmGet("products", ["name", "price"])
+    const result = await client.hIncrBy("products", "price", 100)
+
+    res.json({ result })
+})
+
+
+//sorted sets
+app.get("/sorted-sets", async (req, res) => {
+
+    // const result = await client.zAdd("racer_scores", { score: 12, value: 'Castilla' })
+
+    //     const result = await client.zAdd("racer_scores", [
+    //   { score: 8, value: 'Sam-Bodden' },
+    //   { score: 10, value: 'Royce' },
+    //   { score: 6, value: 'Ford' },
+    //   { score: 14, value: 'Prickett' },
+    //   { score: 12, value: 'Castilla' }
+    // ])
+
+    // const result = await client.zRange('racer_scores', 0, -1)
+    // const result = await client.zRangeWithScores('racer_scores', 0, -1)
+    const result = await client.zRem('racer_scores', 'Castilla')
+
+    res.json({ result })
+})
+
+
+// pub/sub
+
+app.get("/users", async (req, res) => {
+
+    const cachedValue = await client.get("users");
+    if(cachedValue){
+        return res.json(JSON.parse(cachedValue))
+    }
+
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json();
+
+    await client.set("users", JSON.stringify(data))
+    await client.expire('users', 40)
+
+   return res.json({ data })
+
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
